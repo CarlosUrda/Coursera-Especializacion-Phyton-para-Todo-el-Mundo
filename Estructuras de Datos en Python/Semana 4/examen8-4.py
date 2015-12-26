0274593418b2989744e869d783bda68ac3ed5407
Carlos A. Gómez Urda <carlosurda@yahoo.es>
2015-12-26 22:15:38 +0100

    Implementación del Examen de la Semana 4 del Curso 2

---
+++ Estructuras de Datos en Python/Semana 4/examen8-4.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+# Extraer todas las palabras de un archivo y mostrarlas de manera ordenada en
+# una lista.
+
+__author__      = "Carlos A. Gómez Urda"
+__copyright__   = "Copyright 2015"
+__credits__     = ["Carlos A. Gómez Urda"]
+__license__     = "GPL"
+__version__     = "1.0"
+__maintainer__  = "Carlos A. Gómez Urda"
+__email__       = "carlosurda@yahoo.es"
+__status__      = "Producción"
+__date__        = "26/12/2015"
+
+
+nombreDeArchivo = raw_input( "Introduce el archivo: ")
+try:
+    archivo = open( nombreDeArchivo)
+except:
+    print "No existe el archivo", nombreDeArchivo
+    quit()
+
+ordenadas = []
+[ordenadas.append( palabra) for linea in archivo for palabra in linea.split() 
+                                                 if palabra not in ordenadas] 
+
+ordenadas.sort()
+print ordenadas
+
+archivo.close()
